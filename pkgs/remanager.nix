6b3620af5760df15e627265877a0a9cd8bb43af6
Yury Zakharov <colonelcolt@gmail.com>
2026-06-26 01:01:09 +0100

    Fixed hash in reManager package.

--- pkgs/remanager.nix
+++ pkgs/remanager.nix
@@ -16,7 +16,7 @@ let
   version = "1.6.1";
   src = fetchurl {
     url = "https://github.com/rmitchellscott/reManager/releases/download/v${version}/reManager-linux-amd64.tar.gz";
-    hash = "sha256-1v20xl5nm98vwl91bgr54vispxcsdkfgpxpfb9sq8hmdwj4fq05f";
+    hash = "sha256-0spsnnmn7yjrdwfrsv5hgv92wvf0z2wh92n3i7jxzbirn9wy8pnx";
   };
   icon = fetchurl {
     url = "https://raw.githubusercontent.com/rmitchellscott/reManager/v${version}/assets/icon.svg";
